d57750a34670c2b2ac5cb9975268e1e558b2cf4c
amartorell <amartorell@itba.edu.ar>
2020-10-30 14:46:24 -0300

    cosas uart

---
+++ FTM_PWM/source/FTM.c
@@ -0,0 +1,246 @@
+
+#include "FTM.h"
+#include "PORT.h"
+#include "FTM.h"
+#include "gpio.h"
+
+//enum { PA, PB, PC, PD, PE };
+// Convert port and number into pin ID
+// Ex: PTB5  -> PORTNUM2PIN(PB,5)  -> 0x25
+//     PTC22 -> PORTNUM2PIN(PC,22) -> 0x56
+//#define PORTNUM2PIN(p,n)    (((p)<<5) + (n))
+//#define PIN2PORT(p)         (((p)>>5) & 0x07)
+//#define PIN2NUM(p)          ((p) & 0x1F)
+
+
+
+
+void PWM_Init(uint16_t modulos, FTM_Prescal_t prescaler,uint16_t duty);
+//modulus en cantidad de tick totales para obtener el periodo
+//prescaler en el enum definifo como FTM_PSC_XX
+
+
+void PWM_ISR(void);
+
+
+
+
+uint16_t PWM_modulus = 1000-1;
+uint16_t PWM_duty    = 300;//5000-1;
+
+/* FTM0 fault, overflow and channels interrupt handler*/
+__ISR__ FTM0_IRQHandler(void)
+{
+	PWM_ISR();
+}
+
+void PWM_ISR (void)
+{
+	FTM_ClearOverflowFlag (FTM0);
+	//set_DutyPWM(FTM0, 0, percent);
+	//percent +=10;
+	//percent= percent%100;
+
+
+	//FTM_SetCounter(FTM0, 0, PWM_duty++);  //change DC
+	//GPIO_Toggle(PTC, 1 << 8);			  //GPIO pin PTC8
+	//PWM_duty %= PWM_modulus;
+}
+
+
+void FTM_Init (FTM_t ftm)
+{
+
+	if(ftm == FTM0)
+	{
+		SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK;
+		NVIC_EnableIRQ(FTM0_IRQn);
+		FTM0->PWMLOAD = FTM_PWMLOAD_LDOK_MASK | 0x0F;
+	}
+	else if(ftm == FTM1)
+	{
+		SIM->SCGC6 |= SIM_SCGC6_FTM1_MASK;
+		NVIC_EnableIRQ(FTM1_IRQn);
+		FTM1->PWMLOAD = FTM_PWMLOAD_LDOK_MASK | 0x0F;
+	}
+	else if (ftm == FTM2)
+	{
+		SIM->SCGC6 |= SIM_SCGC6_FTM2_MASK;
+		SIM->SCGC3 |= SIM_SCGC3_FTM2_MASK;
+		NVIC_EnableIRQ(FTM2_IRQn);
+		FTM2->PWMLOAD = FTM_PWMLOAD_LDOK_MASK | 0x0F;
+	}
+	else if (ftm == FTM3)
+	{
+		SIM->SCGC3 |= SIM_SCGC3_FTM3_MASK;
+		NVIC_EnableIRQ(FTM3_IRQn);
+		FTM3->PWMLOAD = FTM_PWMLOAD_LDOK_MASK | 0x0F;
+	}
+
+
+	PWM_Init(10000-1,FTM_PSC_x32,70);
+}
+
+/// FTM PWM Example
+
+// To Test Connect PC9(IC)-PC8(GPIO)
+// or PC9(IC)-PC1(OC)
+
+void PWM_Init (uint16_t modulus, FTM_Prescal_t prescaler, uint16_t duty)
+{
+
+	PWM_modulus=modulus;
+	PWM_duty=duty;
+	//seteo los contadores
+
+	FTM0->CNTIN = 0X00;
+	FTM0->MOD = FTM_MOD_MOD(PWM_modulus);
+
+	PORTC->PCR[PIN2NUM(PORTNUM2PIN(PC,1))]=0x00;
+	PORTC->PCR[PIN2NUM(PORTNUM2PIN(PC,1))] |= PORT_PCR_DSE(1);
+	PORTC->PCR[PIN2NUM(PORTNUM2PIN(PC,1))] |= PORT_PCR_MUX(4);
+	PORTC->PCR[PIN2NUM(PORTNUM2PIN(PC,1))] |= PORT_PCR_IRQC(0);
+
+	//	PORT_Configure2 (PORTC,1,UserPCR);
+
+	FTM0->CNT = 0X00;
+
+
+	FTM_SetPrescaler(FTM0, prescaler);
+	FTM_SetModulus(FTM0, PWM_modulus);
+	FTM_SetOverflowMode(FTM0, true);
+	FTM_SetWorkingMode(FTM0, 0, FTM_mPulseWidthModulation);			// MSA  / B
+	FTM_SetPulseWidthModulationLogic(FTM0, 0, FTM_lAssertedHigh);   // ELSA / B
+	set_DutyPWM(FTM0, 0, PWM_duty);
+	//FTM_SetCounter(FTM0, 0, PWM_duty);
+	FTM_StartClock(FTM0);
+}
+
+
+
+
+// Setters
+
+void FTM_SetPrescaler (FTM_t ftm, FTM_Prescal_t data)
+{
+	ftm->SC = (ftm->SC & ~FTM_SC_PS_MASK) | FTM_SC_PS(data);
+}
+
+void FTM_SetModulus (FTM_t ftm, FTMData_t data)
+{
+	ftm->CNTIN = 0X00;
+	ftm->CNT = 0X00;
+	ftm->MOD = FTM_MOD_MOD(data);
+}
+
+FTMData_t FTM_GetModulus (FTM_t ftm)
+{
+	return ftm->MOD & FTM_MOD_MOD_MASK;
+}
+
+void FTM_StartClock (FTM_t ftm)
+{
+	ftm->SC |= FTM_SC_CLKS(0x01);
+}
+
+void FTM_StopClock (FTM_t ftm)
+{
+	ftm->SC &= ~FTM_SC_CLKS(0x01);
+}
+
+void FTM_SetOverflowMode (FTM_t ftm, bool mode)
+{
+	ftm->SC = (ftm->SC & ~FTM_SC_TOIE_MASK) | FTM_SC_TOIE(mode);
+}
+
+bool FTM_IsOverflowPending (FTM_t ftm)
+{
+	return ftm->SC & FTM_SC_TOF_MASK;
+}
+
+void FTM_ClearOverflowFlag (FTM_t ftm)
+{
+	ftm->SC &= ~FTM_SC_TOF_MASK;
+}
+
+void FTM_SetWorkingMode (FTM_t ftm, FTMChannel_t channel, FTMMode_t mode)
+{
+	ftm->CONTROLS[channel].CnSC = (ftm->CONTROLS[channel].CnSC & ~(FTM_CnSC_MSB_MASK | FTM_CnSC_MSA_MASK)) |
+			                      (FTM_CnSC_MSB((mode >> 1) & 0X01) | FTM_CnSC_MSA((mode >> 0) & 0X01));
+}
+
+FTMMode_t FTM_GetWorkingMode (FTM_t ftm, FTMChannel_t channel)
+{
+	return (ftm->CONTROLS[channel].CnSC & (FTM_CnSC_MSB_MASK | FTM_CnSC_MSA_MASK)) >> FTM_CnSC_MSA_SHIFT;
+}
+
+void FTM_SetInputCaptureEdge (FTM_t ftm, FTMChannel_t channel, FTMEdge_t edge)
+{
+	ftm->CONTROLS[channel].CnSC = (ftm->CONTROLS[channel].CnSC & ~(FTM_CnSC_ELSB_MASK | FTM_CnSC_ELSA_MASK)) |
+				                  (FTM_CnSC_ELSB((edge >> 1) & 0X01) | FTM_CnSC_ELSA((edge >> 0) & 0X01));
+}
+
+FTMEdge_t FTM_GetInputCaptureEdge (FTM_t ftm, FTMChannel_t channel)
+{
+	return (ftm->CONTROLS[channel].CnSC & (FTM_CnSC_ELSB_MASK | FTM_CnSC_ELSA_MASK)) >> FTM_CnSC_ELSA_SHIFT;
+}
+
+void FTM_SetOutputCompareEffect (FTM_t ftm, FTMChannel_t channel, FTMEffect_t effect)
+{
+	ftm->CONTROLS[channel].CnSC = (ftm->CONTROLS[channel].CnSC & ~(FTM_CnSC_ELSB_MASK | FTM_CnSC_ELSA_MASK)) |
+				                  (FTM_CnSC_ELSB((effect >> 1) & 0X01) | FTM_CnSC_ELSA((effect >> 0) & 0X01));
+}
+
+FTMEffect_t FTM_GetOutputCompareEffect (FTM_t ftm, FTMChannel_t channel)
+{
+	return (ftm->CONTROLS[channel].CnSC & (FTM_CnSC_ELSB_MASK | FTM_CnSC_ELSA_MASK)) >> FTM_CnSC_ELSA_SHIFT;
+}
+
+void FTM_SetPulseWidthModulationLogic (FTM_t ftm, FTMChannel_t channel, FTMLogic_t logic)
+{
+	ftm->CONTROLS[channel].CnSC = (ftm->CONTROLS[channel].CnSC & ~(FTM_CnSC_ELSB_MASK | FTM_CnSC_ELSA_MASK)) |
+				                  (FTM_CnSC_ELSB((logic >> 1) & 0X01) | FTM_CnSC_ELSA((logic >> 0) & 0X01));
+}
+
+FTMLogic_t FTM_GetPulseWidthModulationLogic (FTM_t ftm, FTMChannel_t channel)
+{
+	return (ftm->CONTROLS[channel].CnSC & (FTM_CnSC_ELSB_MASK | FTM_CnSC_ELSA_MASK)) >> FTM_CnSC_ELSA_SHIFT;
+}
+
+void FTM_SetCounter (FTM_t ftm, FTMChannel_t channel, FTMData_t data)
+{
+	ftm->CONTROLS[channel].CnV = FTM_CnV_VAL(data);
+}
+
+FTMData_t FTM_GetCounter (FTM_t ftm, FTMChannel_t channel)
+{
+	return ftm->CONTROLS[channel].CnV & FTM_CnV_VAL_MASK;
+}
+
+void FTM_SetInterruptMode (FTM_t ftm, FTMChannel_t channel, bool mode)
+{
+	ftm->CONTROLS[channel].CnSC = (ftm->CONTROLS[channel].CnSC & ~FTM_CnSC_CHIE_MASK) | FTM_CnSC_CHIE(mode);
+}
+
+bool FTM_IsInterruptPending (FTM_t ftm, FTMChannel_t channel)
+{
+	return ftm->CONTROLS[channel].CnSC & FTM_CnSC_CHF_MASK;
+}
+
+void FTM_ClearInterruptFlag (FTM_t ftm, FTMChannel_t channel)
+{
+	ftm->CONTROLS[channel].CnSC &= ~FTM_CnSC_CHF_MASK;
+}
+
+void set_DutyPWM(FTM_t ftm,FTMChannel_t Chn, uint16_t  percent)
+{
+	//double duty_per=(percent/100.0)*(PWM_modulus+1);
+	PWM_duty=percent*(PWM_modulus+1)/100;
+	FTM_SetCounter(ftm, Chn, PWM_duty);
+}
+
+
+
+
+
+
---
+++ FTM_PWM/source/gpio.c
@@ -0,0 +1,283 @@
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//	@file		gpio.c										    //
+//	@brief		Simple GPIO Pin services, similar to Arduino	//
+//	@author		Grupo	4										//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//							Headers								//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+#include "gpio.h"
+#include "MK64F12.h"
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//		CONSTANT AND MACRO DEFINITIONS USING #DEFINE 			//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+#define PIN_IRQ_ENABLED		true
+#define PIN_IRQ_DISABLED	false
+#define PIN_IRQ_MASK		0x00000001
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//			ENUMERATIONS AND STRUCTURES AND TYPEDEFS	  		//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+typedef void (*IRQ_callback_t)(void);
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//						STATIC VARIABLES						//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+static GPIO_Type* gpioPtrs[] = GPIO_BASE_PTRS;
+static PORT_Type* portPtrs[] = PORT_BASE_PTRS;
+static uint32_t simMasks[] = {SIM_SCGC5_PORTA_MASK, SIM_SCGC5_PORTB_MASK, SIM_SCGC5_PORTC_MASK, SIM_SCGC5_PORTD_MASK, SIM_SCGC5_PORTE_MASK };
+static uint32_t IRQn_ports[] = {PORTA_IRQn, PORTB_IRQn, PORTC_IRQn, PORTD_IRQn, PORTE_IRQn};
+static uint32_t pin_ports_activated[5];
+static SIM_Type* sim_ptr = SIM;
+static IRQ_callback_t callbacks[5][32];
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS W FILE LEVEL SCOPE//
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+static	void PORT_ClearInterruptFlag(uint32_t port_num, uint32_t pin_num);
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//					FUNCTION DEFINITIONS						//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+void gpioMode (pin_t pin, uint8_t mode){
+/*****************************************************************
+ * @brief Configures the specified pin to behave either as an input or an output
+ * @param pin the pin whose mode you wish to set (according PORTNUM2PIN)
+ * @param mode INPUT, OUTPUT, INPUT_PULLUP or INPUT_PULLDOWN.
+ *****************************************************************/
+	sim_ptr->SCGC5 |= simMasks[PIN2PORT(pin)]; // activo clock gating
+	PORT_Type *port = portPtrs[PIN2PORT(pin)];
+	GPIO_Type *gpio = gpioPtrs[PIN2PORT(pin)];
+	uint32_t num = PIN2NUM(pin); // num es el numero de pin
+
+	// connect to gpio (hay un PCR por pin)
+	port->PCR[num] = 0x00;
+	port->PCR[num] |= PORT_PCR_MUX(1);
+	port->PCR[num] |= PORT_PCR_IRQC(0);
+	switch(mode){
+		case INPUT:
+			gpio->PDDR &= ~ (1<<num); // seteamos el pin como input
+			break;
+		case OUTPUT:
+			gpio->PDDR |= 1<<num;// seteamos el pin como output
+			break;
+		case INPUT_PULLUP:
+			gpio->PDDR &= ~ (1<<num); // seteamos el pin como input
+			port->PCR[num] |= HIGH<<1; //PULL ENABLE en 1
+			port->PCR[num] |= HIGH<<0; //PULL SELECT en 1 (PULLUP)
+			break;
+		case INPUT_PULLDOWN:
+			gpio->PDDR &= ~ (1<<num); // seteamos el pin como input
+			port->PCR[num] |= HIGH<<1; //PULL ENABLE en 1
+			port->PCR[num] &= ~(HIGH<<0); //PULL SELECT en 0 (PULLDOWN)
+			break;
+	}
+	return;
+}
+
+void gpioWrite (pin_t pin, bool value){
+/*****************************************************************
+ * @brief Write a HIGH or a LOW value to a digital pin
+ * @param pin the pin to write (according PORTNUM2PIN)
+ * @param val Desired value (HIGH or LOW)
+ *****************************************************************/
+	uint32_t port_name = PIN2PORT(pin);
+	uint32_t num = PIN2NUM(pin);
+	GPIO_Type *gpio = gpioPtrs[port_name];
+	if(value == HIGH)
+		gpio->PDOR |= (1<<num);
+	else
+		gpio->PDOR &= ~ (1<<num);
+}
+
+void gpioToggle (pin_t pin){
+/*****************************************************************
+ * @brief Toggle the value of a digital pin (HIGH<->LOW)
+ * @param pin the pin to toggle (according PORTNUM2PIN)
+ *****************************************************************/
+	GPIO_Type *gpio = gpioPtrs[PIN2PORT(pin)];
+	gpio->PTOR |= (1<<PIN2NUM(pin));
+}
+
+bool gpioRead (pin_t pin){
+/*****************************************************************
+ * @brief Reads the value from a specified digital pin, either HIGH or LOW.
+ * @param pin the pin to read (according PORTNUM2PIN)
+ * @return HIGH or LOW
+ *****************************************************************/
+	uint32_t port_name = PIN2PORT(pin);
+	uint32_t num = PIN2NUM(pin);
+	GPIO_Type *gpio = gpioPtrs[port_name];
+	return ( (1<<num) & gpio->PDIR ) == 1<<num;
+}
+
+bool gpioIRQ (pin_t pin, uint8_t irqMode, pinIrqFun_t irqFun){
+/*****************************************************************
+ * @brief Configures how the pin reacts when an IRQ event ocurrs
+ * @param pin the pin whose IRQ mode you wish to set (according PORTNUM2PIN)
+ * @param irqMode disable, risingEdge, fallingEdge or bothEdges
+ * @param irqFun function to call on pin event
+ * @return Registration succeed
+ *****************************************************************/
+	uint32_t port_num = PIN2PORT(pin);
+	uint32_t pin_num = PIN2NUM(pin);
+	pin_ports_activated[port_num] |= PIN_IRQ_MASK<<pin_num;
+	callbacks[port_num][pin_num] = irqFun;
+	PORT_Type * addr_arrays[] = PORT_BASE_PTRS;
+	PORT_Type * port = addr_arrays[port_num];
+
+	__NVIC_EnableIRQ(IRQn_ports[port_num]);
+	__NVIC_ClearPendingIRQ(IRQn_ports[port_num]);
+	__NVIC_SetPriority(IRQn_ports[port_num], 3);
+	//Set the corresponding IRQC
+	switch(irqMode){
+			case GPIO_IRQ_MODE_DISABLE:
+				port->PCR[pin_num] &= ~(0b1111 << PORT_PCR_IRQC_SHIFT);
+			case GPIO_IRQ_MODE_RISING_EDGE:
+				port->PCR[pin_num] &= ~(0b1111 << PORT_PCR_IRQC_SHIFT);
+				port->PCR[pin_num] |= 0b1001 << PORT_PCR_IRQC_SHIFT;
+				break;
+			case GPIO_IRQ_MODE_FALLING_EDGE:
+				port->PCR[pin_num] &= ~(0b1111 << PORT_PCR_IRQC_SHIFT);
+				port->PCR[pin_num] |= 0b1010 << PORT_PCR_IRQC_SHIFT;
+				break;
+			case GPIO_IRQ_MODE_BOTH_EDGES:
+				port->PCR[pin_num] &= ~(0b1111 << PORT_PCR_IRQC_SHIFT);
+				port->PCR[pin_num] |= 0b1011 << PORT_PCR_IRQC_SHIFT;
+				break;
+			case GPIO_IRQ_CANT_MODES:
+				break;
+			default:
+				break;
+		}
+	return true;
+}
+
+void gpioDisableIRQ(pin_t pin){
+/*****************************************************************
+ * @brief Disables a previosuly activated interrupt pin
+ * @param pin the pin whose IRQ mode you wish to dishable (according PORTNUM2PIN)
+ *****************************************************************/
+	uint32_t port_num = PIN2PORT(pin);
+	uint32_t pin_num = PIN2NUM(pin);
+	pin_ports_activated[port_num] &= ~(PIN_IRQ_MASK<<pin_num);
+	callbacks[port_num][pin_num] = 0;
+	__NVIC_DisableIRQ(IRQn_ports[port_num]);
+	__NVIC_ClearPendingIRQ(IRQn_ports[port_num]);
+	return;
+}
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//							HANDLERS							//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+void PORTA_IRQHandler(void){
+	for(int i=0; i<32; i++) {
+		//if(pin_ports_activated[PA][i]==PIN_IRQ_ENABLED){
+		if(((PIN_IRQ_MASK<<i) & pin_ports_activated[PA]) != 0x00){
+		// Agarro La pascara 0x00000001 y la shifteo i lugares hacia la iquierda, 
+		//	aplico una and con el registro de 32 bits del puerto en cuestion, y me fijo 
+		// si el resultado es distinto de cero, es decir, me fijo si el pin 'i' esta activado como 
+		// interrupcion.
+			PORT_ClearInterruptFlag(PA,i);
+			callbacks[PA][i]();
+		}
+	}
+	return;
+}
+
+void PORTB_IRQHandler(void){
+	for(int i=0; i<32; i++) {
+		//if(pin_ports_activated[PB][i]==PIN_IRQ_ENABLED){
+		if(((PIN_IRQ_MASK<<i) & pin_ports_activated[PB]) != 0x00){
+		// Agarro La pascara 0x00000001 y la shifteo i lugares hacia la iquierda, 
+		//	aplico una and con el registro de 32 bits del puerto en cuestion, y me fijo 
+		// si el resultado es distinto de cero, es decir, me fijo si el pin 'i' esta activado como 
+		// interrupcion.
+			PORT_ClearInterruptFlag(PB,i);
+			callbacks[PB][i]();
+		}
+	}
+	return;
+}
+
+void PORTC_IRQHandler(void){
+	for(int i=0; i<32; i++) {
+		//if(pin_ports_activated[PC][i]==PIN_IRQ_ENABLED){
+		if(((PIN_IRQ_MASK<<i) & pin_ports_activated[PC]) != 0x00){
+		// Agarro La pascara 0x00000001 y la shifteo i lugares hacia la iquierda, 
+		//	aplico una and con el registro de 32 bits del puerto en cuestion, y me fijo 
+		// si el resultado es distinto de cero, es decir, me fijo si el pin 'i' esta activado como 
+		// interrupcion.
+			PORT_ClearInterruptFlag(PC,i);
+			callbacks[PC][i]();
+		}
+	}
+	return;
+}
+
+void PORTD_IRQHandler(void){
+	for(int i=0; i<32; i++) {
+		//if(pin_ports_activated[PD][i]==PIN_IRQ_ENABLED){
+		if(((PIN_IRQ_MASK<<i) & pin_ports_activated[PD]) != 0x00){
+		// Agarro La pascara 0x00000001 y la shifteo i lugares hacia la iquierda, 
+		//	aplico una and con el registro de 32 bits del puerto en cuestion, y me fijo 
+		// si el resultado es distinto de cero, es decir, me fijo si el pin 'i' esta activado como 
+		// interrupcion.
+			PORT_ClearInterruptFlag(PD,i);
+			callbacks[PD][i]();
+		}
+	}
+	return;
+}
+
+void PORTE_IRQHandler(void){
+	for(int i=0; i<32; i++) {
+		//if(pin_ports_activated[PE][i]==PIN_IRQ_ENABLED){
+		if(((PIN_IRQ_MASK<<i) & pin_ports_activated[PE]) != 0x00){
+		// Agarro La pascara 0x00000001 y la shifteo i lugares hacia la iquierda, 
+		//	aplico una and con el registro de 32 bits del puerto en cuestion, y me fijo 
+		// si el resultado es distinto de cero, es decir, me fijo si el pin 'i' esta activado como 
+		// interrupcion.
+			PORT_ClearInterruptFlag(PE,i);
+			callbacks[PE][i]();
+		}
+	}
+	return;
+}
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//					LOCAL FUNCTION DEFINITIONS					//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+void PORT_ClearInterruptFlag(uint32_t port_num, uint32_t pin_num) {
+	PORT_Type * addr_arrays[] = PORT_BASE_PTRS;
+	PORT_Type * port = addr_arrays[port_num];
+	port->PCR[pin_num] |= PORT_PCR_ISF_MASK;
+	return;
+}
