698263c59abe947ea766f98b0d7184989838c5ca
MD <MD-23@users.noreply.github.com>
2020-11-28 18:31:37 -0300

    Accelerometer

---
+++ source/Drivers/Accelerometer/accelerometer.c
@@ -0,0 +1,192 @@
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//	@file		accelerometer.c   								//
+//	@brief		Accelerometer implementation                    //
+//	@author		Grupo	4										//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//							Headers								//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+#include "accelerometer.h"
+#include "./I2C/I2C.h"
+#include "./Timer/timer.h"
+#include "../../board.h"
+#include "MK64F12.h"
+#include <stddef.h>
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//		CONSTANT AND MACRO DEFINITIONS USING #DEFINE 		 	//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+#define ACCELEROMETER_ADDRESS			0x1D
+#define ACCELEROMETER_SLAVE_ADDRESS 	0x1E // FXOS8700CQ I2C address
+#define ACCELEROMETER_DATA_PACK_LEN		  13 // Status plus 6 channels = 13 bytes
+
+
+//FXOS8700CQ internal register addresses
+#define ACCELEROMETER_STATUS 			0x00
+#define ACCELEROMETER_WHOAMI 			0x0D
+#define ACCELEROMETER_XYZ_DATA_CFG 		0x0E
+#define ACCELEROMETER_CTRL_REG1 		0x2A
+#define ACCELEROMETER_M_CTRL_REG1 		0x5B
+#define ACCELEROMETER_M_CTRL_REG2 		0x5C
+#define ACCELEROMETER_WHOAMI_VAL 		0xC7
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//			ENUMERATIONS AND STRUCTURES AND TYPEDEFS	  		//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//						STATIC VARIABLES						//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+static tim_id_t timer_id;
+static accelerometer_raw_data_t accelerometer_data;
+static accelerometer_raw_data_t magnometer_data;
+static I2C_transcieve_t i2c_com;
+static uint8_t slave_address;
+static uint8_t register_address;
+static uint8_t buffer[ACCELEROMETER_DATA_PACK_LEN]; //Reading Buffer 
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS W FILE LEVEL SCOPE//
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+void dummy(void){};
+
+static void handler();
+/*****************************************************************
+ * @brief Accelerometer Handler, it performs the corresponding 
+ * reading and saving of the accelerometer and magnometer data 
+ ****************************************************************/
+
+
+static accelerometer_error_t configuration();
+/*****************************************************************
+ * @brief Function that configures the accelerometer to enable the
+ * reading of the accelerometer and magnometer data
+ * @returns: Returns I2C communication errors 
+ ****************************************************************/
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//					FUNCTION DEFINITIONS						//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+void accelerometer_init(){
+/*****************************************************************
+ * @brief Function to initialize accelerometer module
+ ****************************************************************/
+
+	I2C_init(I2C_0);
+	configuration();
+	i2c_com.error = I2C_NO_ERROR;
+
+	timer_Init();
+	timer_id = timerGetId();
+	timerStart(timer_id,TIMER_MS2TICKS(100),TIM_MODE_PERIODIC,handler);
+	return;
+}
+
+
+static accelerometer_error_t configuration(){
+/*****************************************************************
+ * @brief Function that configures the accelerometer to enable the
+ * reading of the accelerometer and magnometer data
+ * @returns: Returns accelerometer errors 
+ ****************************************************************/
+
+	//Secuencia de configuracion del acelerometro
+	slave_address = ACCELEROMETER_SLAVE_ADDRESS;
+	i2c_com.callback = dummy;
+	i2c_com.data_size = 1;
+
+	register_address = ACCELEROMETER_CTRL_REG1;
+	i2c_com.data[0] = 0x00;
+
+	while (!I2C_init_transcieve(slave_address,register_address, &i2c_com, false));
+
+	register_address = ACCELEROMETER_CTRL_REG1;
+	i2c_com.data[0] = 0x1F;
+
+	while(!I2C_init_transcieve(slave_address,register_address, &i2c_com, false));
+
+	register_address = ACCELEROMETER_M_CTRL_REG2;
+	i2c_com.data[0] = 0x20;
+
+	while(!I2C_init_transcieve(slave_address,register_address, &i2c_com, false));
+
+	register_address = ACCELEROMETER_XYZ_DATA_CFG;
+	i2c_com.data[0] = 0x01;
+	
+	while(!I2C_init_transcieve(slave_address,register_address, &i2c_com, false));
+
+	register_address = ACCELEROMETER_CTRL_REG1;
+	i2c_com.data[0] = 0x0D;
+
+	while(!I2C_init_transcieve(slave_address,register_address, &i2c_com, false));
+
+	return ACCELEROMETER_NO_ERROR;
+}
+
+
+static void handler(void){
+/*****************************************************************
+ * @brief Accelerometer Handler, it performs the corresponding 
+ * reading and saving of the accelerometer and magnometer data 
+ ****************************************************************/
+
+	slave_address = ACCELEROMETER_SLAVE_ADDRESS;
+	register_address = ACCELEROMETER_STATUS;
+	i2c_com.callback = save_data;
+	i2c_com.data_size = ACCELEROMETER_DATA_PACK_LEN;
+	i2c_com.data = buffer;
+
+	I2C_init_transcieve(slave_address,register_address,&i2c_com, true);
+}
+
+
+static void save_data (void){
+/*****************************************************************
+ * @brief Function that saves accelerometer and magnometer data
+ * in the corresponding structs of data 
+ ****************************************************************/
+
+	accelerometer_data.x = (int16_t)((buffer[1]  << 8) | buffer[2])	>> 2;
+	accelerometer_data.y = (int16_t)((buffer[3]  << 8) | buffer[4])	>> 2;
+	accelerometer_data.z = (int16_t)((buffer[5]  << 8) | buffer[6])	>> 2;
+
+	magnometer_data.x    =			 (buffer[7]  << 8) | buffer[8];
+	magnometer_data.y    =			 (buffer[9]  << 8) | buffer[10];
+	magnometer_data.z    =			 (buffer[11] << 8) | buffer[12];
+}
+
+
+accelerometer_raw_data_t accelerometer_get_data(accelerometer_data_options_t data_option){
+/*****************************************************************
+ * @brief Function to get accelerometer or magnometer data
+ * @param data_option: Data option can be accelerometer or magnometer
+ * @returns accelerometer_raw_data_t: Struct with the x,y,z variables.
+ ****************************************************************/
+
+	if(data_option == ACCELEROMETER_ACCEL_DATA)
+		return accelerometer_data;
+	else 
+		return magnometer_data;
+}
--- source/Tester/App_ Accelerometer.c
+++ source/Tester/App_ Accelerometer.c
@@ -3,7 +3,7 @@
 //							Headers								//
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
-
+#include "./Drivers/Accelerometer/accelerometer.h"
 #include "./Drivers/Timer/timer.h"
 #include "./Drivers/I2C/I2C.h"
 
@@ -13,32 +13,7 @@
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-#define BUFFER_SIZE			10
-#define MAGN_ADRESS_SLAVE	0x1D
-#define TEST_ADDRESS_W		0x26
-#define TEST_ADDRESS_R		0x26
 
-
-
-// FXOS8700CQ internal register addresses 
-#define FXOS8700CQ_STATUS       0x00 
-#define FXOS8700CQ_WHOAMI       0x0D 
-#define FXOS8700CQ_XYZ_DATA_CFG 0x0E 
-#define FXOS8700CQ_CTRL_REG1    0x2A 
-#define FXOS8700CQ_M_CTRL_REG1  0x5B 
-#define FXOS8700CQ_M_CTRL_REG2  0x5C 
-#define FXOS8700CQ_WHOAMI_VAL   0xC7
-
-
-
-#define OUT_X_MSB   0x01
-#define OUT_X_LSB   0x02
-#define OUT_Y_MSB   0x03
-#define OUT_Y_LSB   0x04
-#define OUT_Z_MSB   0x05
-#define OUT_Z_LSB   0x06
-
-#define FXOS8700CQ_READ_LEN 13
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //			ENUMERATIONS AND STRUCTURES AND TYPEDEFS	  		//
@@ -47,19 +22,6 @@
 
 typedef void (* callbackp) (void);
 
-typedef enum { I2C_ERROR,  I2C_OK} I2C_FAIL;
-
-typedef struct{
-    int16_t x;
-    int16_t y;
-    int16_t z;
-}SRAWDATA;
-
-typedef struct{
-    SRAWDATA pMagnData;
-    SRAWDATA pAccelData;
-    I2C_FAIL error;
-}read_data;
 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
@@ -67,11 +29,8 @@ typedef struct{
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-static read_data r_data;
+static  accelerometer_raw_data_t test;;
 
-I2C_COM_CONTROL i2c_com;
-
-uint8_t Buffer[FXOS8700CQ_READ_LEN];
 
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
@@ -79,8 +38,10 @@ uint8_t Buffer[FXOS8700CQ_READ_LEN];
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
-void dummy(void){};
-void accel_magn_dataread(void);
+
+
+
+
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //					FUNCTION DEFINITIONS						//
@@ -88,42 +49,19 @@ void accel_magn_dataread(void);
 //////////////////////////////////////////////////////////////////
 
 void App_Init (void){
-	I2C_init(I2C_0);
+
 	timerInit();
-	r_data.error = I2C_OK;
+	accelerometer_init();
 	return;
 }
 
 void App_Run (void){
-    
-    i2c_com.slave_address = MAGN_ADRESS_SLAVE;
-	i2c_com.data_size = 1;
-	i2c_com.data = Buffer;
-	i2c_com.callback = dummy;
-
-	i2c_com.register_address = FXOS8700CQ_CTRL_REG1;
-	i2c_com.data[0] = 0x00;
-	I2C_init_transcieve(&i2c_com, false);
-	timerDelay(TIMER_MS2TICKS(100));
-
-	i2c_com.register_address = FXOS8700CQ_M_CTRL_REG1;
-	i2c_com.data[0] = 0x1F;
-	I2C_init_transcieve(&i2c_com, false);
-	timerDelay(TIMER_MS2TICKS(100));
-
-	i2c_com.register_address = FXOS8700CQ_M_CTRL_REG2;
-	i2c_com.data[0] = 0x20;
-	I2C_init_transcieve(&i2c_com, false);
-	timerDelay(TIMER_MS2TICKS(100));
-
-	i2c_com.register_address = FXOS8700CQ_XYZ_DATA_CFG;
-	i2c_com.data[0] = 0x01;
-	I2C_init_transcieve(&i2c_com, false);
-	timerDelay(TIMER_MS2TICKS(100));
-	while(true){
-		accel_magn_dataread();
+   
+	while(1){
+		timerDelay(TIMER_MS2TICKS(500));
+		test = accelerometer_get_data(ACCELEROMETER_ACCEL_DATA);
+		return;
 	}
-	return;
 }
 
 //////////////////////////////////////////////////////////////////
@@ -132,28 +70,3 @@ void App_Run (void){
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-
-void accel_magn_dataread(void){
-
-    i2c_com.register_address = OUT_X_MSB;
-    i2c_com.data_size = false;
-    I2C_init_transcieve(&i2c_com,true);
-	timerDelay(TIMER_MS2TICKS(100));
-
-    if(i2c_com.fault == I2C_NO_FAULT){
-    	r_data.pAccelData.x = (int16_t)(((Buffer[0] << 8) | Buffer[1]))>> 2;
-		r_data.pAccelData.y = (int16_t)(((Buffer[2] << 8) | Buffer[3]))>> 2;
-		r_data.pAccelData.z = (int16_t)(((Buffer[4] << 8) | Buffer[5]))>> 2;
-
-        // copy the magnetometer byte data into 16 bit words
-        //r_data->pMagnData.x = (int16_t)(Buffer[7] << 8) | Buffer[8];
-        //r_data->pMagnData.y = (int16_t)(Buffer[9] << 8) | Buffer[10];
-        //r_data->pMagnData.z = (int16_t)(Buffer[11] << 8) | Buffer[12];
-
-        r_data.error = I2C_OK;
-    }
-    else
-        r_data.error = I2C_ERROR;
-
-	return;
-}
